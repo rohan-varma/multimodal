599bc6174fe7bac4166660b4224ad7a43f8dd089
ankitade <de.ankita8@gmail.com>
2022-06-30 07:59:43 +0000

    [FLAVA]Change ordering on contrastive loss initialization
    ghstack-source-id: 8614e1ea8ffd9ef8dc961fe9b0132c6792db74f1
    Pull Request resolved: https://github.com/facebookresearch/multimodal/pull/105

--- torchmultimodal/models/flava/flava_model.py
+++ torchmultimodal/models/flava/flava_model.py
@@ -185,9 +185,8 @@ def flava_model_for_pretraining(
     # TODO: Add parameters for loss here
 ):
     model = flava_model(**flava_model_kwargs)
-
-    codebook = DalleVAEEncoder(image_size=codebook_image_size)
     losses = FLAVAPretrainingLoss()
+    codebook = DalleVAEEncoder(image_size=codebook_image_size)
 
     flava = FLAVAForPreTraining(
         model=model,
@@ -212,7 +211,6 @@ def flava_model_for_classification(
     pretrained_model_key: Optional[str] = "flava_full",
     **flava_model_kwargs: Any,
 ):
-    model = flava_model(**flava_model_kwargs)
     classifier = MLP(
         in_dim=classifier_in_dim,
         out_dim=num_classes,
@@ -221,7 +219,7 @@ def flava_model_for_classification(
         activation=classifier_activation,
         normalization=classifier_normalization,
     )
-
+    model = flava_model(**flava_model_kwargs)
     if loss_fn is None:
         loss_fn = nn.CrossEntropyLoss()
 
